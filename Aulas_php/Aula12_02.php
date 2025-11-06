bd78a28db7bcf526da16e4b1bc0bee8279e52a0d
andreMSUni9 <andre.mendessilva@uni9.edu.br>
2025-11-06 12:37:12 +0000

    Aula 12

---
+++ Aulas_php/Aula12_02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 12 - 6/11</title>
+</head>
+<body>
+    <h2>Aula 12 - 6/11</h2>
+    <?php
+    $cores=["blue","red","green"];
+    print_r($cores);
+    echo "<p>$cores[0]</p>";
+    ?>
+</body>
+</html>
